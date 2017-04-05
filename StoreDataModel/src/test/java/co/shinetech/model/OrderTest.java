56184ea5006bb2173ebfa0852a7b9ac91031e7e8
Marat <teenwoodsman@gmail.com>
2017-04-05 21:39:16 +1200

    add tests for Order

--- StoreDataModel/src/test/java/co/shinetech/model/OrderTest.java
+++ StoreDataModel/src/test/java/co/shinetech/model/OrderTest.java
@@ -2,20 +2,95 @@ package co.shinetech.model;
 
 import static org.junit.Assert.*;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class OrderTest {
 
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void testSetAndGetId() {
+		long id = 1;
+		LongPK pk = new LongPK(id);
+		Order order = new Order();
+		
+		order.setId(pk);
+		
+		assertEquals(id, order.getId().getId());
 	}
 	
 	@Test
-	public void checkEquals() {
-		Order o1 = new Order(new LongPK(1L));
-		Order o2 = new Order(new LongPK(2L));
+	public void testSetAndGetDate() {
+		ZoneId zoneId = ZoneId.of("NZ");
+		ZonedDateTime date = ZonedDateTime.of(2017, 04, 01, 12, 30, 00, 0, zoneId);
+		String dateAsString = date.toString();
+		Order order = new Order();
 		
-		assertFalse(o1.equals(o2));
+		order.setDate(date);
+		
+		assertEquals(dateAsString, order.getDate().toString());
+	}
+	
+	@Test
+	public void testSetAndGetEstimatedDeliveryDate() {
+		ZoneId zoneId = ZoneId.of("NZ");
+		ZonedDateTime deliveryDate = ZonedDateTime.of(2017, 04, 05, 12, 30, 00, 0, zoneId);
+		String deliveryDateAsString = deliveryDate.toString();
+		Order order = new Order();
+		
+		order.setEstimatedDelivery(deliveryDate);
+		
+		assertEquals(deliveryDateAsString, order.getEstimatedDelivery().toString());
+	}
+	
+	@Test
+	public void testSetAndGetOrderStatus() {
+		OrderStatus orderStatus = OrderStatus.CART;
+		Order order = new Order();
+		
+		order.setOrderStatus(orderStatus);
+		
+		assertEquals(orderStatus, order.getOrderStatus());
+	}
+	
+	@Test
+	public void testSetAndGetPaymentMethod() {
+		PaymentMethod paymentMethod = PaymentMethod.CASH;
+		Order order = new Order();
+		
+		order.setPaymentMethod(paymentMethod);
+		
+		assertEquals(paymentMethod, order.getPaymentMethod());
+	}
+	
+	@Test
+	public void testSetAndGetCustomer() {
+		Customer customer = new Customer();
+		Order order = new Order();
+		
+		order.setCustomer(customer);
+		
+		assertEquals(customer, order.getCustomer());
+	}
+	
+	//TODO think about more suitable test
+	@Test
+	public void testAddAndGetItems() {
+		OrderItem item1 = new OrderItem();
+		OrderItem item2 = new OrderItem();
+		List<OrderItem> listToAdd = new ArrayList();
+		listToAdd.add(item1);
+		listToAdd.add(item2);
+		Order order = new Order();
+		
+		order.addItems(listToAdd);
+		
+		Object[] arrayToGet = order.getItemsStream().toArray();
+		
+		assertEquals(item1, arrayToGet[0]);
+		assertEquals(item2, arrayToGet[1]);
 	}
 }
