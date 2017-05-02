1014bc2f8df3637c0bfb1779a712fe8adc9494e2
Java Programming 101 <henrywright88404@gmail.com>
2017-05-02 19:45:37 +1200

    Added orderItem tests

---
+++ StoreDataModel/src/test/java/co/shinetech/model/OrderItemTest.java
@@ -0,0 +1,74 @@
+package co.shinetech.model;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Marc on 2/05/2017.
+ */
+public class OrderItemTest {
+
+    @Test
+    public void testGetSetID (){
+        OrderItem oi = new OrderItem();
+        LongPK lpk = new LongPK(7L);
+
+        oi.setId(lpk);
+
+        assertEquals(lpk, oi.getId());
+    }
+
+    @Test
+    public void testGetSetQuantity(){
+        OrderItem oi = new OrderItem();
+        int qty = 3;
+
+        oi.setQty(qty);
+
+        assertEquals(qty, oi.getQty());
+    }
+
+    @Test
+    public void testGetSetPrice(){
+        OrderItem oi = new OrderItem();
+        BigDecimal price = new BigDecimal(39.33);
+
+        oi.setPrice(price);
+
+        assertEquals(price, oi.getPrice());
+    }
+
+    @Test
+    public void testGetSetOrder(){
+        OrderItem oi = new OrderItem();
+
+        List<OrderItem> orderItemList = new ArrayList<>();
+
+        Order order  = new Order( new LongPK(4l), new Date(2017,05,10), new Date (2017,05,11), OrderStatus.PROCESSED,
+                PaymentMethod.CASH, new Customer(), orderItemList);
+
+
+        oi.setOrder(order);
+
+        assertEquals(order.toString(), oi.getOrder().toString());
+    }
+
+    @Test
+    public void testGetSetProduct(){
+        OrderItem oi = new OrderItem();
+
+        Product product = new Product();
+        product.setId(new LongPK(7L));
+
+        oi.setProduct(product);
+
+        assertEquals(product, oi.getProduct());
+
+    }
+}
