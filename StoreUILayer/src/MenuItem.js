9c50bb587ab2b9392c774172dfc6b9b981b1ca3a
DinizR <rodrigo.dinis@outlook.com>
2017-08-04 13:04:47 +1200

    Fixed the menu issue and filled data in database (sql) for categories.

--- StoreUILayer/src/MenuItem.js
+++ StoreUILayer/src/MenuItem.js
@@ -3,20 +3,8 @@ import SubMenu from './SubMenu';
 import 'whatwg-fetch';
 
 class MenuItem extends Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.state = {};
-    }
-
-    handleChange(e) {
-        this.props.categories = e.props.value;
-    }
-
     render() {
         return (
-          console.log("MenuItem category props ===", this.props.categories),
-          console.log("MenuItem category state ===", this.state.categories),
             <li className="grid"><a className={this.props.class} href={this.props.href}>{this.props.caption}</a>
                 <SubMenu categories={this.props.category.children} />
             </li>
